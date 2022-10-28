3d38cf16a794789307b4876b7434a44e36ca1c2f
ThomasJacqueminet <th.jacqueminet@gmail.com>
2022-10-28 15:46:06 +0200

    ModalCharactersInfo | Create and get data

---
+++ src/components/Character/ModalCharacterInfos.js
@@ -0,0 +1,61 @@
+import { Box, Modal, Typography } from '@mui/material';
+import CakeIcon from '@mui/icons-material/Cake';
+import ChurchIcon from '@mui/icons-material/Church';
+import MaleIcon from '@mui/icons-material/Male';
+import FemaleIcon from '@mui/icons-material/Female';
+import * as React from 'react';
+
+const ModalCharactersInfo = (props) => {
+    const {setOpenModal, openModal, character} = props 
+    return (
+        <Modal
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        >
+            <Box sx={modaleStyle}>
+                <Typography id="modal-modal-title" variant="h4" component="h2">
+                    {character.gender == 'Male' ? <MaleIcon/> : <FemaleIcon/> }
+                    {character.name ? character.name : character.aliases[0]}
+                </Typography>
+
+                {character.aliases ? 
+                (
+                <Typography variant="h5" component="h3">
+                    {character.aliases[0]}
+                </Typography>
+                ): ''}
+                {character.titles ? 
+                (
+                <Typography variant="h5" component="h3">
+                    {character.titles[0]}
+                </Typography>
+                ): ''}
+                {character.born ?
+                (
+                <Typography sx={{ mt: 2 }} component="h6">
+                    <CakeIcon/> Naissance : {character.born}
+                </Typography>) : '' }
+
+                {character.died ?
+                (
+                <Typography sx={{ mt: 2 }} component="h6">
+                    <ChurchIcon/> Décès : {character.died}
+                </Typography>) : '' }
+            </Box>
+        </Modal>
+    )
+}
+
+const modaleStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+  };
+export default ModalCharactersInfo;
--- src/components/Character/index.js
+++ src/components/Character/index.js
@@ -1,22 +1,29 @@
 import { Button, Card, CardActions, CardContent, Typography } from "@mui/material";
 import InfoIcon from '@mui/icons-material/InfoOutlined';
+import { useState } from "react";
+import ModalCharactersInfo from "./ModalCharacterInfos";
 
 const Character = (props) => {
     const {character} = props 
+    const [openModal, setOpenModal] = useState(false)
 
     return (
         <Card sx={{ maxWidth: 345 }}>
             <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {character.name ? character.name : character.aliases[0]}
+                <Typography variant="h5" component="div">
+                    {character.name ? character.name : character.aliases[0]} 
+                </Typography>
+                <Typography variant="body2" gutterBottom  >
+                    {character.name ? character.aliases[0] : ''}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
                     {character.titles[0]}
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size="small"><InfoIcon/> Détails du personnage</Button>
+                <Button size="small" onClick={() => setOpenModal(true)} ><InfoIcon/> Détails du personnage</Button>
             </CardActions>
+            <ModalCharactersInfo setOpenModal={setOpenModal} openModal={openModal} character={character}/>
         </Card>
     )
 }
